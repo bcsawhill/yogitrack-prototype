f94988204b5fed7fd80729c098027dab1849a386
ClarksMyName <ryanclark16123@gmail.com>
2026-02-26 14:44:54 -0500

    first commit

---
+++ yogiserver.cjs
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+app.use(express.static(path.join(__dirname, "Public")));
+
+// Serve static files from the public dir
+app.use(express.static("public"));
+app.use(express.json());
+
+app.use("/api/instructor", require("./routes/instructorRoutes.cjs"));
+
+
+
+// Start the web server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}...`);
+  console.log('Open http://localhost:8080/index.html in your browser to view the app.');
+});
